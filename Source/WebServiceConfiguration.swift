a490895bc5b2a3e9407ca32ddb65a2324d386f2f
Roy McKenzie <roy@propellerlabs.co>
2017-02-09 16:43:16 -0800

    user can provide no base path on a webservice configuration.

--- Source/WebServiceConfiguration.swift
+++ Source/WebServiceConfiguration.swift
@@ -12,9 +12,19 @@ public enum WebServiceConfigurationError: Error {
     case couldNotBuildUrl
 }
 
+extension WebServiceConfigurationError: LocalizedError {
+    
+    public var errorDescription: String? {
+        switch self {
+        case .couldNotBuildUrl:
+            return NSLocalizedString("Could not build URL", comment: "")
+        }
+    }
+}
+
 /// Protocol for configuring a `URLRequest` for a resource credentials on a request
 public struct WebServiceConfiguration {
-    public let basePath: String
+    public let basePath: String?
     public let additionalHeaders: [String: String]?
     public var credential: WebServiceConfigurationCredential?
     
